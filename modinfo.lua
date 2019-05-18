db6f856f4d0e71b7656d4e36265be23e3082effe
_Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2019-05-18 10:22:12 -0700

    Add spring ignore / modinfo

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+  name='Zero-K Evolved',
+  description='Zero-K Evolved',
+  shortname='ZKE',
+  version='v0.1 Beta',
+  mutator='1',
+  game='Zero-K',
+  shortGame='ZK',
+  modtype=1,
+  depend = {
+    'Spring content v1',
+	'Zero-K v1.7.5.1',
+  },
+}
